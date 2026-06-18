5530a12ec26f843e94dfd66a2b5acb5f3d84e557
Swapnil-77 <swapnilphad07@gmail.com>
2026-06-18 21:27:14 +0530

    Fix endpoint mapping from '/searchh' to '/search'

--- src/main/java/com/swapnil/Train/controller/TrainSearchController.java
+++ src/main/java/com/swapnil/Train/controller/TrainSearchController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/searchh")
+@RequestMapping("/search")
 @CrossOrigin
 public class TrainSearchController {
 
