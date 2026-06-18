0f0289ababc2bfa81d9c7014e065a892c4923ba0
Swapnil-77 <swapnilphad07@gmail.com>
2026-06-18 21:27:01 +0530

    Update request mapping path to '/searchh'

--- src/main/java/com/swapnil/Train/controller/TrainSearchController.java
+++ src/main/java/com/swapnil/Train/controller/TrainSearchController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/search")
+@RequestMapping("/searchh")
 @CrossOrigin
 public class TrainSearchController {
 
