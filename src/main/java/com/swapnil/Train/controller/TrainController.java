b214fd5f02e2f36c2ae238996ee4f9e6d7cda9bf
Swapnil-77 <swapnilphad07@gmail.com>
2026-07-11 22:25:30 +0530

    Change train endpoint from '/train' to '/trains'

--- src/main/java/com/swapnil/Train/controller/TrainController.java
+++ src/main/java/com/swapnil/Train/controller/TrainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/train")
+@RequestMapping("/trains")
 public class TrainController {
 
     private TrainService trainService;
